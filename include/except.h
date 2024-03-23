1d0b554ab65b7d0d589d07f54929a5ed0f6cbcd4
Randy <randyjordan@email.com>
2024-03-23 14:32:48 +0000

    Macros for the interface

--- include/except.h
+++ include/except.h
@@ -19,5 +19,34 @@ typedef struct ExceptFrame ExceptFrame;
 enum { EXCEPT_ENTERED, EXCEPT_RAISED, EXCEPT_HANDLED, EXCEPT_FINALIZED};
 void hard_fail(const char* fmt, ...);
 
+#define RAISE(e) except_raise(&(e), __FILE__, __LINE__)
+#define RERAISE except_raise(ExceptFrame.exception, \
+	ExceptFrame.file, ExceptFrame.line)
+#define RETURN switch (except_stack = except_stack->prev,0) default: return
+#define TRY do { \
+	volatile int except_flag; \
+	ExceptFrame except_frame; \
+	except_frame.prev = except_stack; \
+	except_stack = &except_frame;  \
+	except_flag = setjmp(except_frame.env); \
+	if (except_flag == EXCEPT_ENTERED) {
+#define EXCEPT(e) \
+		if (except_flag == EXCEPT_ENTERED) except_stack = except_stack->prev; \
+	} else if (except_frame.exception == &(e)) { \
+		except_flag = EXCEPT_HANDLED;
+#define ELSE \
+		if (except_flag == EXCEPT_ENTERED) except_stack = except_stack->prev; \
+	} else { \
+		except_flag = EXCEPT_HANDLED;
+#define FINALLY \
+		if (except_flag == EXCEPT_ENTERED) except_stack = except_stack->prev; \
+	} { \
+		if (except_flag == EXCEPT_ENTERED) \
+			except_flag = EXCEPT_FINALIZED;
+#define END_TRY \
+		if (except_flag == EXCEPT_ENTERED) except_stack = except_stack->prev; \
+		} if (except_flag == EXCEPT_RAISED) RERAISE; \
+} while (0)
+
 
 #endif
